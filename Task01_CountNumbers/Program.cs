21b28eaf116fea70f39ca8fc39997985c673a393
NKompa <nkompa7@gmail.com>
2022-09-09 01:08:33 +0300

    Count numbers greater than 0

--- Task01_CountNumbers/Program.cs
+++ Task01_CountNumbers/Program.cs
@@ -1,19 +1,13 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь: 0, 7, 8, -2, -2 -> 2; 1, -7, 567, 89, 223-> 3.
 
-int[] arrayNum = new int[6];
-int count = 0;
+int[] Numbers = new int[6];
+int countNumbers = 0;
 
 Console.WriteLine("Введите 6 чисел:");
 
-arrayNum[0] = Convert.ToInt32(Console.ReadLine());
-arrayNum[1] = Convert.ToInt32(Console.ReadLine());
-arrayNum[2] = Convert.ToInt32(Console.ReadLine());
-arrayNum[3] = Convert.ToInt32(Console.ReadLine());
-arrayNum[4] = Convert.ToInt32(Console.ReadLine());
-arrayNum[5] = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 0; i < arrayNum.Length; i++)
+for (int i = 0; i < Numbers.Length; i++)
 {
-    if (arrayNum[i] > 0) count++;
+    Numbers[i] = Convert.ToInt32(Console.ReadLine());
+    if (Numbers[i] > 0) countNumbers++;
 }
-Console.WriteLine($"Чисел больше ноля: {count}");
+Console.WriteLine($"Чисел больше ноля: {countNumbers}");
